a4bc9ac19b4810ae45ab9a8c0d9fc1ab81502d11
drquantum12 <arjunsinghtomar03511@gmail.com>
2023-03-13 15:33:23 +0530

    added login and homepage

---
+++ login/login2.js
@@ -0,0 +1,6 @@
+function validation(){
+    //get data from input box
+    var user= document.getElementById('user').value;
+    var pass= document.getElementById('pass').value;
+    console.log(user,pass);
+}
